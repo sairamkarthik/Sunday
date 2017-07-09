890eba5b2cf0b38ff13249ec1bcf90b81d1eee6c
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-09 14:33:10 +0530

    Add files via upload

---
+++ ideone_zgBtj1.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		System.out.println("Enter number:"+n);
+		int a;
+		a=(n*(n+1))/2;
+		System.out.println("Sum:"+a);
+		// your code goes here
+	}
+}
