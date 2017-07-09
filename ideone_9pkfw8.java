1c599a8df829aa710a0a21462468132c896d4882
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-09 12:03:50 +0530

    Add files via upload

---
+++ ideone_9pkfw8.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a number");
+x=s.nextInt();
+if(x%2==0)
+{
+    System.out.println("It is an even number");
+}
+
+else 
+{
+    System.out.println("It is an odd number");
+}		// your code goes here
+	}
+}
